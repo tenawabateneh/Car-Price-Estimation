58a63de961e533ec62f77d6d8568aea3114ad93a
tenaw <tenawabateneh06@gmail.com>
2025-04-09 18:56:24 +0300

    Persisting Data with TypeORM and Sqlite DB well done

--- ReadMe.txt
+++ ReadMe.txt
@@ -14,3 +14,11 @@ nest g service reports
 // To create Controllers
 nest g controller users
 nest g controller reports
+
+
+// To install TYPEORM and Sqlite
+npm install @nestjs/typeorm typeorm sqlite3
+
+
+// To install a class-validator package and use it in a dto
+npm install class-validator class-transformer
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { ReportEntity } from './report.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReportEntity])],
   providers: [ReportsService],
   controllers: [ReportsController]
 })
-export class ReportsModule {}
+export class ReportsModule { }
