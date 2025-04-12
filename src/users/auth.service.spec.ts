2643866cf0b71e955c0fbdc5dc5eadabede814d8
tenaw <tenawabateneh06@gmail.com>
2025-04-12 11:07:46 +0300

    Getting Started with Unit Testing (Getting TypeScript to Help With Mocks)

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,14 +2,15 @@ import { Test } from "@nestjs/testing";
 
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
+import { UserEntity } from "./user.entity";
 
 
 it("Can create an instance of AuthService", async () => {
   // Create a fake copy of UsersService
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password })
+      Promise.resolve({ id: 1, email, password } as UserEntity)
   };
 
   const module = await Test.createTestingModule({
