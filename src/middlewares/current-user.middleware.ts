cca778f2e538e475dbf69adbcd26a3c72825dd07
tenaw <tenawabateneh06@gmail.com>
2025-05-03 08:37:46 +0300

    A Basic Permissions System (fixing a type defination error)

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -2,6 +2,17 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 
 import { UsersService } from "src/users/users.service";
+import { UserEntity } from "src/users/user.entity";
+
+// IMPORTANT: This must be top-level in the module
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserEntity | null;
+    }
+  }
+}
+
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -13,8 +24,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      // @ts-ignore
-      req.currentUser = user;
+      req.currentUser = user; // <-- now no error
     }
 
     next();
