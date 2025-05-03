31df6e654d3f648df347952574c508b2b431d058
tenaw <tenawabateneh06@gmail.com>
2025-05-03 10:08:12 +0300

    A Basic Permissions System (Transforming Query String Data)

--- src/reports/dtos/get-estimate-price.dto.ts
+++ src/reports/dtos/get-estimate-price.dto.ts
@@ -1,3 +1,4 @@
+import { Transform } from "class-transformer"
 import { IsLatitude, IsLongitude, IsNumber, IsString, Max, Min } from "class-validator"
 
 export class GetEstimatePriceDto {
@@ -7,19 +8,23 @@ export class GetEstimatePriceDto {
   @IsString()
   model: string
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
   mileage: number
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLongitude()
   lng: number
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLatitude()
   lat: number
 }
